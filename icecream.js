af0b4ed49865587bbf9d31106bb08bfd426304e9
SAKITHA S <sakithakumar04@gmail.com>
2024-10-04 22:30:58 +0530

    Create icecream.js

---
+++ icecream.js
@@ -0,0 +1,26 @@
+.container {
+    width: 80%;
+    padding: 20px;
+    background-color: white;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    
+  }
+  #inventory li {
+    background-color: #eaeaea;
+    padding: 10px;
+    font-size: 18px;
+  }
+  button {
+    display: inline-block;
+    padding: 10px 15px;
+    font-size: 16px;
+    border: none;
+    border-radius: 4px;
+    background-color: #4caf50;
+    color: white;
+  }
+  button:hover {
+    background-color: #45a049;
+  }
+  
+ 
